a2531ccdad164d048f91969876a6e7d39cc969e1
kanekodaichi <knkdic.bfc@gmail.com>
2021-02-16 13:52:17 +0900

    ボウリングプログラムオブジェクト指向版&テスト作成

---
+++ 07.bowling_object/bowling_object.rb
@@ -0,0 +1,102 @@
+#! /usr/bin/env ruby
+# frozen_string_literal: true
+
+class Game
+  def initialize(score_text)
+    @frames = []
+    frame = []
+
+    score_text.chars.each do |s|
+      frame << s
+      next unless @frames.size < 9
+
+      if frame.size >= 2 || s == 'X'
+        @frames << Frame.new(frame[0], frame[1])
+        frame.clear
+      end
+    end
+    # 最終フレームは3投いれる
+    @frames << Frame.new(frame[0], frame[1], frame[2])
+  end
+
+  # フレームごとの通常スコア
+  def calc_frame_score(num)
+    target_frame = @frames[num]
+    target_frame.frame_score
+  end
+
+  def strike?(num)
+    @frames[num].first_shot.mark == 'X'
+  end
+
+  def next_strike?(num)
+    @frames[num + 1].first_shot.mark == 'X'
+  end
+
+  def supea?(num)
+    @frames[num].first_shot.score + @frames[num].second_shot.score == 10
+  end
+
+  # 全てのフレームの合計
+  def score
+    point = 0
+    10.times.each do |num|
+      point += calc_frame_score(num)
+      point += calc_bonus_score(num)
+    end
+    point
+  end
+
+  # ボーナス得点の計算
+  def calc_bonus_score(num)
+    bonus = 0
+    if num == 9
+      bonus += 0
+    elsif num == 8 && strike?(num) && next_strike?(num)
+      bonus += 10 + @frames[num + 1].second_shot.score
+
+    elsif strike?(num) && next_strike?(num)
+      bonus += 10 + @frames[num + 2].first_shot.score
+
+    elsif strike?(num)
+      bonus += @frames[num + 1].first_shot.score + @frames[num + 1].second_shot.score
+
+    elsif supea?(num)
+      bonus += @frames[num + 1].first_shot.score
+    end
+    bonus
+  end
+end
+
+class Frame
+  attr_reader :first_shot, :second_shot, :third_shot
+
+  def initialize(first_mark, second_mark = nil, third_mark = nil)
+    @first_shot = Shot.new(first_mark)
+    @second_shot = Shot.new(second_mark)
+    @third_shot = Shot.new(third_mark)
+  end
+
+  # ボーナス加点を含まないフレームごとのスコア
+  def frame_score
+    [first_shot.score, second_shot.score, third_shot.score].sum
+  end
+end
+
+class Shot
+  attr_reader :mark
+
+  def initialize(mark)
+    @mark = mark
+  end
+
+  # 文字列を整数に変換
+  def score
+    return 10 if mark == 'X'
+
+    mark.to_i
+  end
+end
+
+game = Game.new(ARGV[0])
+p game.score
---
+++ 07.bowling_object/bowling_object_test.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require 'minitest/autorun'
+require './bowling_object'
+
+class TestGame < MiniTest::Unit::TestCase
+  def test_score1
+    game_score1 = Game.new('6390038273X9180X645')
+    assert_equal 139, game_score1.score
+  end
+
+  def test_score2
+    game_score2 = Game.new('6390038273X9180XXXX')
+    assert_equal 164, game_score2.score
+  end
+
+  def test_score3
+    game_score3 = Game.new('0X150000XXX518104')
+    assert_equal 107, game_score3.score
+  end
+
+  def test_score4
+    game_score4 = Game.new('6390038273X9180XX00')
+    assert_equal 134, game_score4.score
+  end
+
+  def test_score5
+    game_score5 = Game.new('XXXXXXXXXXXX')
+    assert_equal 300, game_score5.score
+  end
+end
